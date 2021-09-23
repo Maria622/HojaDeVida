e54f2e6c71231efe57a36f030b0790285c6a07f6
Maria622 <mariacamilaangarita.20@gmail.com>
2021-09-23 18:04:24 -0500

    fin

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,10 +1,18 @@
+import {
+    Link
+  } from "react-router-dom";
+
+
 function Error404(){
 
 	return(
-	<div className="container">
+	<div>
+    <div className="container">
          Error 404 Estás intentando acceder a una página que no existe
       </div>
-		);
+      <center><button className="btn btn-outline-primary"><Link to="home">Ir al Inicio</Link></button></center>
+	</div>
+    );
 }
 
 export default Error404;
